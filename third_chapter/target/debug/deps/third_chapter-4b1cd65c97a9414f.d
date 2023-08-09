cc16e8c666b79beb4a27854e217c3bf44fdf1432
carlos zaragoza beato <carloszaragozabeato15@gmail.com>
2023-08-09 17:35:25 +0200

    08-09

---
+++ third_chapter/target/debug/deps/third_chapter-4b1cd65c97a9414f.d
@@ -0,0 +1,5 @@
+/home/carlos/Desktop/RUSTUPAO/third_chapter/target/debug/deps/third_chapter-4b1cd65c97a9414f: src/main.rs
+
+/home/carlos/Desktop/RUSTUPAO/third_chapter/target/debug/deps/third_chapter-4b1cd65c97a9414f.d: src/main.rs
+
+src/main.rs:
---
+++ third_chapter/target/debug/third_chapter.d
@@ -0,0 +1,1 @@
+/home/carlos/Desktop/RUSTUPAO/third_chapter/target/debug/third_chapter: /home/carlos/Desktop/RUSTUPAO/third_chapter/src/main.rs
