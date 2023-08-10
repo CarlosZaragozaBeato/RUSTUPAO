65dc23e6cd4f44e447c37c491b4b0e949e88330b
carlos zaragoza beato <carloszaragozabeato15@gmail.com>
2023-08-10 17:18:05 +0200

    10-08

--- third_chapter/src/main.rs
+++ third_chapter/src/main.rs
@@ -109,6 +109,37 @@ fn main() {
 
     // FUNCTIONS
 
+    // println!("HELLO WORLD");
+    
+    // another_function(121);
+    // print_labeled_measurement(5,'h');
+
+    // let y = {
+    //     let x = 3;
+    //     x + 1
+    // };
+    // println!("The value of y is: {y}");
+    // let x = five();
+    // println!("The value of x is: {x}");
+
+    let x = plus_one(5);
+    println!("The value of x is: {x}");
+
+}
+fn plus_one(x:i32) -> i32 {
+    x + 1
+}
+
+fn five() -> i32 {
+    5
+}
 
 
+fn print_labeled_measurement(value:i32, unit_label:char) { 
+    println!("The measurement is : {value}{unit_label}");
+}
+
+
+fn another_function (x:i64){
+    println!("I DONT KNOW JAMBO: {x}");
 }
