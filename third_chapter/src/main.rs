96d8705239aa278427c25d4f3072096143c15ae0
carlos zaragoza beato <carloszaragozabeato15@gmail.com>
2023-08-12 09:51:54 +0200

    12-08

--- third_chapter/src/main.rs
+++ third_chapter/src/main.rs
@@ -122,20 +122,114 @@ fn main() {
     // let x = five();
     // println!("The value of x is: {x}");
 
-    let x = plus_one(5);
-    println!("The value of x is: {x}");
+    // let x = plus_one(5);
+    // println!("The value of x is: {x}");
+
+    let number = 3;
+
+    // if number < 5 {
+    //     println!("condiction was true");
+    // } else {
+    //     println!("condiction was false");
+    // }
+
+
+    // if number != 0 {
+    //     println!("The number is different than 0");
+    // }
+
+
+    // if number % 4 == 0 {
+    //     println!("Number is divisible by 4");
+    // } else if number % 3 ==0 {
+    //     println!("Number is divisible by 3");
+    // } else if number % 2 == 0 {
+    //     println!("Number is divisible by 2");
+    // } else {
+    //     println!("Number is not divisible by 4,3,2");
+    // }
+
+
+    // let condition = true;
+    // let number = if condition{ 5 } else { 6 };
+
+    // println!("The value of number is: {number}");
+    
+
+
+    // let mut counter = 0;
+    // let result = loop {
+    //     println!("Inside the loop {counter}");
+    //     if counter == 10 {
+    //         break counter *2;
+    //     }
+    //     counter += 1;
+    // };
+    // println!("Outside the loop {result}");
+
+
+    // let mut count = 0;
+    // 'counting_up: loop {
+    //     println!("count = {count}");
+        
+    //     let mut remaining = 10;
+
+    //     loop {
+    //         println!("remaining = {remaining}");
+        
+    //         if remaining == 9 {
+    //             break;
+    //         }
+
+    //         if count == 2 {
+    //             break 'counting_up;
+    //         }
+    //         remaining -=1;
+    //     }
+    //     count += 1;
+    // }
+    // println!("End count = {count}");
+
+
+    // let mut number = 3;
+    // while number != 0 {
+    //     println!("{number}");
+
+    //     number -= 1;
+    // }
+    // println!("LITOFF!!!");
+
+
+    // let a = [10,20,30,40,50];
+    // let mut index = 0;
+    // while index < 5 { 
+    //     println!("The value is: {}", a[index]);
+    //     index += 1;
+    // }
+
+    // let a = [10,20,30,40,50];
+    // for element in a { 
+    //     println!("The value is {element}");
+    // }
+
+    for number in (1..4).rev() {
+        println!("{number}");
+    }
+    println!("LITOFF!!!");
+
+
 
 }
-fn plus_one(x:i32) -> i32 {
-    x + 1
-}
-
+// fn plus_one(x:i32) -> i32 {
+//     x + 1
+// }
+/* 
 fn five() -> i32 {
     5
 }
+ */
 
-
-fn print_labeled_measurement(value:i32, unit_label:char) { 
+/* fn print_labeled_measurement(value:i32, unit_label:char) { 
     println!("The measurement is : {value}{unit_label}");
 }
 
@@ -143,3 +237,4 @@ fn print_labeled_measurement(value:i32, unit_label:char) {
 fn another_function (x:i64){
     println!("I DONT KNOW JAMBO: {x}");
 }
+ */
